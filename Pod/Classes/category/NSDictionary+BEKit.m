7b53bea604ab69e0ea03a5391aaf27bdf7bcc779
孙丹龙 <sundanlong@gmail.com>
2016-07-22 14:39:05 +0800

    添加NSDictionary默认值

--- Pod/Classes/category/NSDictionary+BEKit.m
+++ Pod/Classes/category/NSDictionary+BEKit.m
@@ -15,7 +15,15 @@
     return [NSDictionary be_dictionaryToJSON:self];
 }
 
+- (NSString * _Nonnull)be_dictionaryToJSON:(NSString * _Nonnull)defaultValue {
+    return [NSDictionary be_dictionaryToJSON:self defaultValue:defaultValue];
+}
+
 + (NSString * _Nonnull)be_dictionaryToJSON:(NSDictionary * _Nonnull)dictionary {
+    return [self be_dictionaryToJSON:dictionary defaultValue:@"{}"];
+}
+
++ (NSString * _Nonnull)be_dictionaryToJSON:(NSDictionary * _Nonnull)dictionary defaultValue:(NSString * _Nonnull)defaultValue {
     NSString * result = nil;
     if ([dictionary be_isValid]) {
         @try {
@@ -32,42 +40,66 @@
             
         }
     }
-    return result ? result : @"{}";
+    return result ? result : defaultValue;
 }
 
 - (NSArray * _Nonnull)be_arrayObjectForKey:(NSString * _Nonnull)key {
     return [NSDictionary be_arrayObject:self forKey:key];
 }
 
+- (NSArray * _Nonnull)be_arrayObjectForKey:(NSString * _Nonnull)key defaultValue:(NSArray * _Nonnull)defaultValue {
+    return [NSDictionary be_arrayObject:self forKey:key defaultValue:defaultValue];
+}
+
 + (NSArray * _Nonnull)be_arrayObject:(NSDictionary * _Nonnull)dictionary forKey:(NSString * _Nonnull)key {
+    return [self be_arrayObject:dictionary forKey:key defaultValue:@[]];
+}
+
++ (NSArray * _Nonnull)be_arrayObject:(NSDictionary * _Nonnull)dictionary forKey:(NSString * _Nonnull)key defaultValue:(NSArray * _Nonnull)defaultValue {
     if (dictionary) {
         id valueObj = [dictionary objectForKey:key];
         if (valueObj && valueObj != [NSNull null] && [valueObj isKindOfClass:[NSArray class]]) {
             return (NSArray *)valueObj;
         }
     }
-    return [NSArray array];
+    return defaultValue;
 }
 
 - (NSDictionary * _Nonnull)be_dictionaryObjectForKey:(NSString * _Nonnull)key {
     return [NSDictionary be_dictionaryObject:self forKey:key];
 }
 
+- (NSDictionary * _Nonnull)be_dictionaryObjectForKey:(NSString * _Nonnull)key defaultValue:(NSDictionary * _Nonnull)defaultValue {
+    return [NSDictionary be_dictionaryObject:self forKey:key defaultValue:defaultValue];
+}
+
 + (NSDictionary * _Nonnull)be_dictionaryObject:(NSDictionary * _Nonnull)dictionary forKey:(NSString * _Nonnull)key {
+    return [self be_dictionaryObject:dictionary forKey:key defaultValue:@{}];
+}
+
++ (NSDictionary * _Nonnull)be_dictionaryObject:(NSDictionary * _Nonnull)dictionary forKey:(NSString * _Nonnull)key defaultValue:(NSDictionary * _Nonnull)defaultValue {
     if (dictionary) {
         id valueObj = [dictionary objectForKey:key];
         if (valueObj && valueObj != [NSNull null] && [valueObj isKindOfClass:[NSDictionary class]]) {
             return (NSDictionary *)valueObj;
         }
     }
-    return [NSDictionary dictionary];
+    return defaultValue;
 }
 
 - (NSString * _Nonnull)be_stringObjectForKey:(NSString * _Nonnull)key {
     return [NSDictionary be_stringObject:self forKey:key];
 }
 
+- (NSString * _Nonnull)be_stringObjectForKey:(NSString * _Nonnull)key defaultValue:(NSString * _Nonnull)defaultValue {
+    return [NSDictionary be_stringObject:self forKey:key defaultValue:defaultValue];
+}
+
 + (NSString * _Nonnull)be_stringObject:(NSDictionary * _Nonnull)dictionary forKey:(NSString * _Nonnull)key {
+    return [self be_stringObject:dictionary forKey:key defaultValue:@""];
+}
+
++ (NSString * _Nonnull)be_stringObject:(NSDictionary * _Nonnull)dictionary forKey:(NSString * _Nonnull)key defaultValue:(NSString * _Nonnull)defaultValue {
     if (dictionary) {
         @try {
             id valueObj = [dictionary objectForKey:key];
@@ -78,20 +110,28 @@
             }
         }
         @catch (NSException *exception) {
-            return @"";
+            return defaultValue;
         }
         @finally {
             
         }
     }
-    return @"";
+    return defaultValue;
 }
 
 - (NSNumber * _Nonnull)be_numberObjectForKey:(NSString * _Nonnull)key {
     return [NSDictionary be_numberObject:self forKey:key];
 }
 
+- (NSNumber * _Nonnull)be_numberObjectForKey:(NSString * _Nonnull)key defaultValue:(NSNumber * _Nonnull)defaultValue {
+    return [NSDictionary be_numberObject:self forKey:key defaultValue:defaultValue];
+}
+
 + (NSNumber * _Nonnull)be_numberObject:(NSDictionary * _Nonnull)dictionary forKey:(NSString * _Nonnull)key {
+    return [self be_numberObject:dictionary forKey:key defaultValue:@(0)];
+}
+
++ (NSNumber * _Nonnull)be_numberObject:(NSDictionary * _Nonnull)dictionary forKey:(NSString * _Nonnull)key defaultValue:(NSNumber * _Nonnull)defaultValue {
     if (dictionary) {
         @try {
             id valueObj = [dictionary objectForKey:key];
@@ -103,13 +143,13 @@
             }
         }
         @catch (NSException *exception) {
-            return [NSNumber numberWithDouble:0];
+            return defaultValue;
         }
         @finally {
             
         }
     }
-    return [NSNumber numberWithDouble:0];
+    return defaultValue;
 }
 
 @end
